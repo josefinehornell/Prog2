b3ab43e264d896892401df98fb1f9a9e157c210d
Josefine Hörnell <josefine.hornell@gmail.com>
2023-10-11 19:34:03 +0200

    ,,

--- MA4/MA4_Files/person.cpp
+++ MA4/MA4_Files/person.cpp
@@ -9,9 +9,8 @@ class Person{
 		int fib();
 	private:
 		int age;
-		int fib_(int);
 	};
-	
+ 
 Person::Person(int n){
 	age = n;
 	}
@@ -19,34 +18,34 @@ Person::Person(int n){
 int Person::get(){
 	return age;
 	}
- 
+
+int _fib(int n){
+	if (n <= 1){
+		return n;
+	}else{
+		return (_fib(n-1) + _fib(n-2));
+	}
+}
+
+int Person::fib(){
+	int n = get();
+	return _fib(n);	
+}
+
 void Person::set(int n){
 	age = n;
 	}
 
-int Person::_fib(int n){
-	if (n <= 1){
-		return n; 
-	}
-	else {
-		return(_fib(n-1) + _fib(n-2));
-	}
-}		
-
-int Person::fib(){
-	int n = get();
-	return _fib(n);
-}
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
 			person = nullptr;
 			}
-		}	
+		}
 	}
-
